598dbd10371696a48864b4899a55f325b87a25f4
chanelleassaad <assaadchanelle@gmail.com>
2024-10-08 16:39:43 +0300

    Submit login on enter

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -3,7 +3,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
 import { MatTab, MatTabGroup } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
-import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import {
+  MatError,
+  MatFormField,
+  MatLabel,
+  MatSuffix,
+} from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
@@ -24,6 +29,7 @@ import { CommonModule } from '@angular/common';
     MatButton,
     MatError,
     MatLabel,
+    MatSuffix,
   ],
 })
 export class LoginModule {}
